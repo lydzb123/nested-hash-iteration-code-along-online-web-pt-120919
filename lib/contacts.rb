c3958aad49d8045d1a6320b86120d8d36edc9ba1
Lydia B <lydiabaik@gmail.com>
2019-12-21 22:28:01 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-contacts.each {|person, contact_details_hash|
+contacts.each {|person, contact_details_hash|}
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry end
